ffc5846ac696eed3c36b63e3bc65cd215d05187b
zcochran <fuzzylimes@gmail.com>
2018-08-04 21:07:30 -0400

    Completed problem
    I can't believe I forgot about using regex...

--- ecmascript/run-length-encoding/run-length-encoding.js
+++ ecmascript/run-length-encoding/run-length-encoding.js
@@ -2,44 +2,6 @@ const encode = (toEncode) => {
     if (!toEncode) {
         return toEncode;
     } else {
-        // let final = '';
-        // let last = '';
-        // let count = 0;
-        // let letters = toEncode.split('');
-        // // console.log(letters);
-        // for (let i = 0; i < letters.length; i++) {
-        //     // console.log(letters[i]);
-        //     if (i === 0) {
-        //         last = letters[i];
-        //         count += 1;
-        //     }  else if (i === letters.length - 1) {
-        //         if (letters[i] === last) {
-        //             count += 1;
-        //             if (count > 1) {
-        //                 final += `${count}${last}`;
-        //             } else {
-        //                 final += `${last}${letters[i]}`;
-        //             }
-        //         } else {
-        //             if (count > 1) {
-        //                 final += `${count}${last}${letters[i]}`;
-        //             } else {
-        //                 final += `${last}${letters[i]}`;
-        //             }
-        //         }
-        //     } else if (letters[i] === last) {
-        //         count += 1;
-        //     } else {
-        //         if (count > 1) {
-        //             final += `${count}${last}`;
-        //         } else {
-        //             final += `${last}`;
-        //         }
-        //         count = 1;
-        //         last = letters[i]
-        //     }
-        // }
-        // console.log(final);
         let encode = '';
         let matches = toEncode.match(/(.)\1*/g);
         for (let i of matches) {
@@ -56,7 +18,6 @@ const decode = (toDecode) => {
     if (!toDecode) {
         return toDecode;
     }
-    //let list = toDecode.match(/\d*\D/g);
     let myRegex = /(\d*)(\D)/g;
     let decode = '';
     let match;
@@ -70,28 +31,6 @@ const decode = (toDecode) => {
             decode += match[2];
         }
     }
-
-    // console.log(list);
-    // for (let i of list) {
-    //     let a = myRegex.exec(i);
-    //     console.log(a);
-    //     if (a.length > 2) {
-    //         for (let j = parseInt(a[1]); j > 0; j--) {
-    //             decode += a[2];
-    //         }
-    //     } else {
-    //         decode += a[1];
-    //     }
-    // }
-
-
-    // for (let i of list) {
-    //     if (i.length > 1) {
-    //         decode += `${i.length}`;
-    //     }
-    //     decode += `${i[0]}`;
-    // }
-    // return decode;
     return decode;
 };
 
