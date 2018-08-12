28717b70f21222db752d57aac232c19cd2f07907
zcochran <fuzzylimes@gmail.com>
2018-08-12 14:36:28 -0400

    Some dumb example outside of Exercism.

---
+++ java/helloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    // Class variables
+    static String randomString = "String to print";
+    // Constant definition
+    static final double PINUM = 3.121529;
+
+    public static void main(String[] args) {
+        System.out.println(PINUM);
+        int integerOne = 22;
+        int integerTwo = integerOne + 3;
+        System.out.println(integerTwo);
+    }
+}
