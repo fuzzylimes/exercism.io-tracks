9a851737ba2c7da0db25d1bfc591ed91baf13126
zcochran <fuzzylimes@gmail.com>
2018-08-21 20:04:45 -0400

    Updating the example a little bit

--- java/SideStuff/Polymorphism/src/main/java/Animal.java
+++ java/SideStuff/Polymorphism/src/main/java/Animal.java
@@ -9,9 +9,11 @@ public class Animal {
         Animal bob = new Animal();
         Cat billy = new Cat();
         Dog john = new Dog();
+        Cow joe = new Cow();
 
         System.out.println(bob.makesSound());
         System.out.println(billy.makesSound());
         System.out.println(john.makesSound());
+        System.out.println(joe.makesSound());
     }
 }
---
+++ java/SideStuff/Polymorphism/src/main/java/Cow.java
@@ -0,0 +1,7 @@
+public class Cow extends Animal {
+
+    public static void main(String[] args) {
+        Cow joe = new Cow();
+        System.out.println(joe.makesSound());
+    }
+}
